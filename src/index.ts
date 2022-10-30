5ac3be971c13969f0750be7c883e65a0860bd0f8
Tim Dillon <tim@itsdits.com>
2022-10-30 09:52:04 +0900

    fix: loading cover not removed on Link/Viber

--- src/index.ts
+++ src/index.ts
@@ -12,18 +12,4 @@ script.src = "https://html5gameportal.com/embeds/wortal-1.2.0.js";
 script.onload = () => Wortal.init();
 document.head.appendChild(script);
 
-// Make sure we have the loading cover added to prevent the game canvas from being shown before the preroll ad finishes.
-if (document.readyState === "loading") {
-    document.addEventListener('DOMContentLoaded', addLoadingCover);
-} else {
-    addLoadingCover();
-}
-
-function addLoadingCover(): void {
-    let cover = document.createElement("div");
-    cover.id = "loading-cover";
-    cover.style.cssText = "background: #000000; width: 100%; height: 100%; position: fixed; z-index: 100;";
-    document.body.prepend(cover);
-}
-
 export default Wortal;
