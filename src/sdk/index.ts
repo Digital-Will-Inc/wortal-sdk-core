e23306084cc996aedc8bd53c5b90d30020f2ad2b
Tim Dillon <tim@itsdits.com>
2022-10-30 09:54:37 +0900

    fix: loading cover not removed on Link/Viber (#15)

--- src/sdk/index.ts
+++ src/sdk/index.ts
@@ -1,5 +1,6 @@
 import * as _ads from './ads';
 import * as _analytics from './analytics';
+import * as _leaderboard from './leaderboard';
 import {InitializationOptions} from "../types/initialization";
 import {PlacementType} from "../types/ad-instance";
 import SDKData from "../utils/sdk";
@@ -13,6 +14,10 @@ export const ads = _ads;
  */
 export const analytics = _analytics;
 /**
+ * Leaderboard API
+ */
+export const leaderboard = _leaderboard;
+/**
  * SDK data container
  */
 export const sdk = new SDKData();
@@ -25,6 +30,19 @@ export function init(options: InitializationOptions = {}): void {
     console.log("[Wortal] Initializing SDK Core..");
     sdk.init();
 
+    // Make sure we have the loading cover added to prevent the game canvas from being shown before the preroll ad finishes.
+    if (document.readyState === "loading") {
+        // The DOMContentLoaded event fires later than expected on Link/Viber, so we should have the <body> available
+        // to us here. On other platforms it may not be available yet, so we'll wait for the event to add it.
+        if (sdk.session.platform === "link" || sdk.session.platform === "viber") {
+            addLoadingCover();
+        } else {
+            document.addEventListener('DOMContentLoaded', addLoadingCover);
+        }
+    } else {
+        addLoadingCover();
+    }
+
     (window as any).initWortal(() => {
         console.log("[Wortal] Platform: " + sdk.session.platform);
         if (sdk.session.platform === "link" || sdk.session.platform === "viber") {
@@ -32,6 +50,7 @@ export function init(options: InitializationOptions = {}): void {
             if ((window as any).wortalGame) {
                 (window as any).wortalGame.initializeAsync().then(() => {
                     (window as any).wortalGame.startGameAsync();
+                    sdk.lateInit();
                     analytics.logGameStart();
                     console.log("[Wortal] SDK Core initialization complete.");
                 });
@@ -39,6 +58,7 @@ export function init(options: InitializationOptions = {}): void {
         } else if (sdk.session.platform === "wortal") {
             ads.showInterstitial(PlacementType.PREROLL, "Preroll", () => {}, () => {
                 removeLoadingCover();
+                sdk.lateInit();
                 sdk.adConfig.setPrerollShown(true);
                 analytics.logGameStart();
                 console.log("[Wortal] SDK Core initialization complete.");
@@ -46,12 +66,14 @@ export function init(options: InitializationOptions = {}): void {
         } else {
             console.log("[Wortal] Entering debug mode..");
             removeLoadingCover();
+            sdk.lateInit();
             analytics.logGameStart();
             console.log("[Wortal] SDK Core initialization complete.");
         }
     }, () => {
         console.log("[Wortal] Ad blocker detected.");
         removeLoadingCover();
+        sdk.lateInit();
         sdk.adConfig.setAdBlocked(true);
         analytics.logGameStart();
         console.log("[Wortal] SDK Core initialization complete.");
@@ -74,10 +96,14 @@ export function setLoadingProgress(value: number): void {
     }
 }
 
-/**
- * Removes the loading cover that prevents the game from being rendered before a preroll ad has shown.
- */
-export function removeLoadingCover(): void {
+function addLoadingCover(): void {
+    let cover = document.createElement("div");
+    cover.id = "loading-cover";
+    cover.style.cssText = "background: #000000; width: 100%; height: 100%; position: fixed; z-index: 100;";
+    document.body.prepend(cover);
+}
+
+function removeLoadingCover(): void {
     if (document.getElementById("loading-cover")) {
         document.getElementById("loading-cover")!.style.display = "none";
     }
