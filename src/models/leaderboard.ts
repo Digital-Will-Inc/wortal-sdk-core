9cd0173fbeadcc745994d4e1119e3212a1b20532
Tim Dillon <tim@itsdits.com>
2022-10-31 16:42:36 +0900

    refactor: leaderboard calls get a new board every call
    * In order to use the Link/Viber API on the Leaderboard object it was easier to just nest the getLeaderboardAsync call within the desired API call, ensuring we can operate on the returned object.

--- src/models/leaderboard.ts
+++ src/models/leaderboard.ts
@@ -1,6 +1,3 @@
-import LeaderboardEntry from "./leaderboard-entry";
-import {sdk} from "../sdk";
-
 /** @hidden */
 interface LeaderboardData {
     id: number;
@@ -39,88 +36,5 @@ export default class Leaderboard {
      */
     get contextId(): string {
         return this._current.contextId;
-    }
-
-    /**
-     * Gets a list of leaderboard entries in the leaderboard.
-     * @param count Number of entries to get.
-     * @param offset Offset from the first entry (top rank) to start the count from. Default is 0.
-     * @returns Array of LeaderboardEntry.
-     */
-    getEntriesAsync(count: number, offset: number = 0): Promise<LeaderboardEntry[]> {
-        if (sdk.session.platform === "link" || sdk.session.platform === "viber") {
-            return (window as any).wortalGame
-                .getEntriesAsync(count, offset)
-                .then((results: any[]) => results.map(result => new LeaderboardEntry(result)))
-                .catch((error: any) => console.error(error));
-        } else {
-            return Promise.reject("[Wortal] Leaderboards not currently supported on platform: " + sdk.session.platform);
-        }
-    }
-
-    /**
-     * Gets the player's entry in the leaderboard.
-     * @returns LeaderboardEntry for the player.
-     */
-    getPlayerEntryAsync(): Promise<LeaderboardEntry> {
-        if (sdk.session.platform === "link" || sdk.session.platform === "viber") {
-            return (window as any).wortalGame
-                .getPlayerEntryAsync()
-                .then((result: any) => new LeaderboardEntry(result))
-                .catch((error: any) => console.error(error));
-        } else {
-            return Promise.reject("[Wortal] Leaderboards not currently supported on platform: " + sdk.session.platform);
-        }
-    }
-
-    /**
-     * Gets the total number of entries in the leaderboard.
-     * @returns Number of entries.
-     */
-    getEntryCountAsync(): Promise<number> {
-        if (sdk.session.platform === "link" || sdk.session.platform === "viber") {
-            return (window as any).wortalGame
-                .getEntryCountAsync()
-                .then((result: any) => result)
-                .catch((error: any) => console.error(error));
-        } else {
-            return Promise.reject("[Wortal] Leaderboards not currently supported on platform: " + sdk.session.platform);
-        }
-    }
-
-    /**
-     * Gets a list of leaderboard entries of connected players in the leaderboard.
-     * @param count Number of entries to get.
-     * @param offset Offset from the first entry (top rank) to start the count from. Default is 0.
-     * @returns Array of LeaderboardEntry.
-     */
-    getConnectedPlayersEntriesAsync(count: number, offset: number): Promise<LeaderboardEntry[]> {
-        if (sdk.session.platform === "link" || sdk.session.platform === "viber") {
-            return (window as any).wortalGame
-                .getConnectedPlayerEntriesAsync(count, offset)
-                .then((results: any[]) => results.map(result => new LeaderboardEntry(result)))
-                .catch((error: any) => console.error(error));
-        } else {
-            return Promise.reject("[Wortal] Leaderboards not currently supported on platform: " + sdk.session.platform);
-        }
-    }
-
-    /**
-     * Sends an entry to be added to the leaderboard, or updated if already existing. Will only update if the score
-     * is a higher than the player's previous entry.
-     * @param score Score for the entry.
-     * @param details Optional additional details about the entry.
-     * @returns The new entry if one was created, updated entry if the score is higher, or the old entry if no new
-     * high score was achieved.
-     */
-    sendEntryAsync(score: number, details: string = ""): Promise<LeaderboardEntry> {
-        if (sdk.session.platform === "link" || sdk.session.platform === "viber") {
-            return (window as any).wortalGame
-                .setScoreAsync()
-                .then((result: any) => new LeaderboardEntry(result))
-                .catch((error: any) => console.error(error));
-        } else {
-            return Promise.reject("[Wortal] Leaderboards not currently supported on platform: " + sdk.session.platform);
-        }
     }
 }
