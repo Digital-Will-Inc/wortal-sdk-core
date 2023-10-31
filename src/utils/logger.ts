823a743fe19ea4a4498b2a91e16d3b0da6223e7d
Tim Dillon <tim@itsdits.com>
2023-10-31 16:15:52 +0900

    refactor(utils): change log styling for cleaner logs

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,72 +1,40 @@
-const logPrefix: string = "[Wortal]";
-const callLogPrefix: string = "[API]";
-const internalCallLogPrefix: string = "[INTERNAL]";
-const performanceLogPrefix: string = "[PERFORMANCE]";
-const statusLogPrefix: string = "[STATUS]";
+const logPrefix: string = " Wortal ";
+const callLogPrefix: string = " API ";
+const internalCallLogPrefix: string = " INTERNAL ";
+const performanceLogPrefix: string = " PERFORMANCE ";
+const statusLogPrefix: string = " STATUS ";
 
 const logTextStyle: string = `font-weight: normal;`;
 
 const logPrefixStyle: string = `
     background: #8258A4;
-    border: 1px solid #CFB86C;
     border-radius: 5px;
     color: white;
     font-weight: bold;`;
 
 const logCallTagStyle: string = `
     background: #A45882;
-    border: 1px solid #CFB86C;
     border-radius: 5px;
     color: white;
     font-weight: bold;`;
 
 const logInternalTagStyle: string = `
     background: #5F5666;
-    border: 1px solid #CFB86C;
     border-radius: 5px;
     color: white;
     font-weight: bold;`;
 
 const logPerformanceTagStyle: string = `
     background: #75AA00;
-    border: 1px solid #CFB86C;
     border-radius: 5px;
     color: white;
     font-weight: bold;`;
 
 const logStatusTagStyle: string = `
     background: #007FFF;
-    border: 1px solid #CFB86C;
     border-radius: 5px;
     color: white;
     font-weight: bold;`;
-
-const infoStyle: string = `
-    background: #8258A4;
-    border: 1px solid #CFB86C;
-    border-radius: 5px;
-    color: white;
-    display: block;
-    width: 100%;
-`;
-
-const warnStyle: string = `
-    background: #CFB86C;
-    border: 1px solid #CFB86C;
-    border-radius: 5px;
-    color: #8258A4;
-    display: block;
-    width: 100%;
-`;
-
-const errorStyle: string = `
-    background: #E81123;
-    border: 1px solid white;
-    border-radius: 5px;
-    color: white;
-    display: block;
-    width: 100%;
-`;
 
 /**
  * Logs a debug message to the console. Logs at the verbose level.
@@ -84,7 +52,7 @@ export function debug(message: string, args?: unknown): void {
  * @hidden
  */
 export function info(message: string, args?: unknown) {
-    console.info(`%c${logPrefix} ${message}`, infoStyle);
+    console.info(`%c${logPrefix}%c ${message}`, logPrefixStyle, logTextStyle);
     if (args !== undefined) {
         console.info(args);
     }
@@ -95,7 +63,7 @@ export function info(message: string, args?: unknown) {
  * @hidden
  */
 export function warn(message: string, args?: unknown) {
-    console.warn(`%c${logPrefix} ${message}`, warnStyle);
+    console.warn(`%c${logPrefix}%c ${message}`, logPrefixStyle, logTextStyle);
     if (args !== undefined) {
         console.warn(args);
     }
@@ -106,7 +74,7 @@ export function warn(message: string, args?: unknown) {
  * @hidden
  */
 export function exception(message: string, args?: unknown) {
-    console.error(`%c${logPrefix} ${message}`, errorStyle);
+    console.error(`%c${logPrefix}%c ${message}`, logPrefixStyle, logTextStyle);
     if (args !== undefined) {
         console.error(args);
     }
