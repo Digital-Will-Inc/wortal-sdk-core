61b101a0efa0d46b91fbef7bf36de1e3c779ab4e
Tim Dillon <tim@itsdits.com>
2023-11-09 17:41:20 +0900

    wip(iap): add iap impl for yandex

--- src/core/interfaces/yandex-sdk.ts
+++ src/core/interfaces/yandex-sdk.ts
@@ -1,4 +1,7 @@
 import { AdCallbacks_Yandex, AdStatus_Yandex } from "../../ads/interfaces/yandex-ads";
+import { PurchaseConfig } from "../../iap/interfaces/purchase-config";
+import { Product_Yandex } from "../../iap/interfaces/yandex-product";
+import { Purchase_Yandex } from "../../iap/interfaces/yandex-purchase";
 
 /**
  * Yandex SDK interface
@@ -6,6 +9,7 @@ import { AdCallbacks_Yandex, AdStatus_Yandex } from "../../ads/interfaces/yandex
  */
 export interface YandexSDK {
     init(): Promise<YandexSDK>;
+    getPayments(params: {signed: true}): Promise<YandexIAPObject>;
     features: {
         LoadingAPI?: {
             ready(): void;
@@ -18,4 +22,16 @@ export interface YandexSDK {
         showBannerAdv(): void;
         hideBannerAdv(): void;
     }
+}
+
+/**
+ * Object used by Yandex SDK to access the In-App Purchases API.
+ * https://yandex.com/dev/games/doc/en/sdk/sdk-purchases
+ * @hidden
+ */
+export interface YandexIAPObject {
+    purchase(purchaseConfig: PurchaseConfig): Promise<Purchase_Yandex>;
+    getCatalog(): Promise<Product_Yandex[]>;
+    getPurchases(): Promise<Purchase_Yandex[]>;
+    consumePurchase(token: string): Promise<void>;
 }
