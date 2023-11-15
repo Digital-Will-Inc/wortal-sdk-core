9d5591601169a4c5bb793d3b58f0dcb48f51b6e6
Tim Dillon <tim@itsdits.com>
2023-11-15 17:26:55 +0900

    feat(core): add stats api

--- src/stats/stats-api.ts
+++ src/stats/stats-api.ts
@@ -1,3 +1,6 @@
+import { GetStatsPayload } from "./interfaces/get-stats-payload";
+import { PostStatsPayload } from "./interfaces/post-stats-payload";
+import { Stats } from "./interfaces/stats";
 import { StatsBase } from "./stats-base";
 
 /**
@@ -10,5 +13,46 @@ export class StatsAPI {
 
     constructor(impl: StatsBase) {
         this._stats = impl;
+    }
+
+    /**
+     * Gets a player's stats.
+     * @param level The name of the level to get stats for.
+     * @param payload Payload with additional details about the stats.
+     * @example
+     * Wortal.stats.getStatsAsync("Level 1")
+     *    .then((stats) => {
+     *      console.log(stats);
+     *    });
+     * @returns {Promise<Stats[]>} Promise that resolves to an array of stats.
+     * @throws {ErrorMessage} See error.message for details.
+     * <ul>
+     * <li>NOT_SUPPORTED</li>
+     * <li>INVALID_PARAMS</li>
+     * </ul>
+     */
+    public getStatsAsync(level: string | number, payload?: GetStatsPayload): Promise<Stats[]> {
+        return this._stats.getStatsAsync(level, payload);
+    }
+
+    /**
+     * Posts a player's stats.
+     * @param level The name of the level the stats are for.
+     * @param value The value of the stat.
+     * @param payload Payload with additional details about the stats.
+     * @example
+     * Wortal.stats.postStatsAsync("Level 1", "100")
+     *   .then(() => {
+     *      console.log("Stats posted successfully");
+     *   });
+     * @returns {Promise<void>} Promise that resolves when the stats have been posted.
+     * @throws {ErrorMessage} See error.message for details.
+     * <ul>
+     * <li>NOT_SUPPORTED</li>
+     * <li>INVALID_PARAMS</li>
+     * </ul>
+     */
+    public postStatsAsync(level: string | number, value: string | number, payload?: PostStatsPayload): Promise<void> {
+        return this._stats.postStatsAsync(level, value, payload);
     }
 }
--- src/stats/stats-base.ts
+++ src/stats/stats-base.ts
@@ -1,5 +1,15 @@
+import { API_URL, WORTAL_API } from "../data/core-data";
+import { invalidParams } from "../errors/error-handler";
+import { ValidationResult } from "../errors/interfaces/validation-result";
+import { apiCall } from "../utils/logger";
+import { isValidNumber, isValidString } from "../utils/validators";
+import { GetStatsPayload } from "./interfaces/get-stats-payload";
+import { PostStatsPayload } from "./interfaces/post-stats-payload";
+import { Stats } from "./interfaces/stats";
+
 /**
  * Base implementation for the Stats API. Extend this class to implement the Stats API for a specific platform.
+ * @hidden
  */
 export abstract class StatsBase {
     constructor() {
@@ -7,11 +17,59 @@ export abstract class StatsBase {
 
 //#region Public API
 
+    public getStatsAsync(level: string | number, payload?: GetStatsPayload): Promise<Stats[]> {
+        apiCall(WORTAL_API.STATS_GET_STATS);
+
+        const validationResult = this.validateGetStats(level, payload);
+        if (!validationResult.valid) {
+            return Promise.reject(validationResult.error);
+        }
+
+        return this.getStatsAsyncImpl(level, payload);
+    }
+
+    public postStatsAsync(level: string | number, value: string | number, payload?: PostStatsPayload): Promise<void> {
+        apiCall(WORTAL_API.STATS_POST_STATS);
+
+        const validationResult = this.validatePostStats(level, value, payload);
+        if (!validationResult.valid) {
+            return Promise.reject(validationResult.error);
+        }
+
+        return this.postStatsAsyncImpl(level, value, payload);
+    }
+
 //#endregion
 //#region Implementation interface
 
+    protected abstract getStatsAsyncImpl(level: string | number, payload?: GetStatsPayload): Promise<any>;
+    protected abstract postStatsAsyncImpl(level: string | number, value: string | number, payload?: PostStatsPayload): Promise<any>;
+
 //#endregion
 //#region Validation
+
+    protected validateGetStats(level: string | number, payload?: GetStatsPayload): ValidationResult {
+        if (!isValidString(level) && !isValidNumber(level)) {
+            return {
+                valid: false,
+                error: invalidParams(undefined, WORTAL_API.STATS_GET_STATS, API_URL.STATS_GET_STATS),
+            };
+        }
+
+        return { valid: true };
+    }
+
+    protected validatePostStats(level: string | number, value: string | number, payload?: PostStatsPayload): ValidationResult {
+        if ((!isValidString(level) && !isValidNumber(level)) ||
+            (!isValidString(value) && !isValidNumber(value))) {
+            return {
+                valid: false,
+                error: invalidParams(undefined, WORTAL_API.STATS_POST_STATS, API_URL.STATS_POST_STATS),
+            };
+        }
+
+        return { valid: true };
+    }
 
 //#endregion
 }
