09730ddaea5dfd383b8f17b29eed2ecbdf80ed44
Marcel B. Daryanto <marcel.budiono@gmail.com>
2023-11-16 09:48:10 +0700

    revert changes to package.json, tsconfig.json, webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,67 +2,65 @@ const path = require('path');
 const webpack = require('webpack');
 const TerserPlugin = require('terser-webpack-plugin');
 
-module.exports = (env, argv) => {
-    console.log(`Build Mode: ${argv.mode}`)
-    return {
-        mode: argv.mode,
-        entry: {
-            sdk: path.resolve(__dirname, 'src/index.ts')
+const IS_PRODUCTION = process.env.NODE_ENV === 'production';
+
+module.exports = {
+    mode: IS_PRODUCTION ? 'production' : 'development',
+    entry: {
+        sdk: path.resolve(__dirname, 'src/index.ts')
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: `wortal-core.js`,
+        chunkFilename: '[name].js',
+        publicPath: '',
+        library: {
+            root: 'Wortal',
+            amd: 'wortal-sdk-core',
+            commonjs: 'wortal-sdk-core'
         },
-        output: {
-            path: path.join(__dirname, 'dist'),
-            filename: `wortal-core.js`,
-            chunkFilename: '[name].js',
-            publicPath: '',
-            library: {
-                root: 'Wortal',
-                amd: 'wortal-sdk-core',
-                commonjs: 'wortal-sdk-core'
-            },
-            libraryTarget: 'umd',
-            libraryExport: 'default'
-        },
-        resolve: {
-            extensions: ['.ts', '.js', '.json']
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.(ts|js)$/,
-                    exclude: /node_modules\//,
-                    use: [
-                        'ts-loader'
-                    ]
-                }
-            ]
-        },
-        optimization: {
-            splitChunks: {
-                cacheGroups: {
-                    shared: {
-                        name: 'wortal-common',
-                        minChunks: 2,
-                    },
+        libraryTarget: 'umd',
+        libraryExport: 'default'
+    },
+    resolve: {
+        extensions: ['.ts', '.js', '.json']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)$/,
+                exclude: /node_modules\//,
+                use: [
+                    'ts-loader'
+                ]
+            }
+        ]
+    },
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                shared: {
+                    name: 'wortal-common',
+                    minChunks: 2,
                 },
             },
-            minimize: true,
-            minimizer: [
-                new TerserPlugin({
-                    parallel: true,
-                    terserOptions: {
-                        format: {
-                            comments: false
-                        },
-                    },
-                    extractComments: false,
-                }),
-            ],
         },
-        plugins: [
-            new webpack.DefinePlugin({
-                __VERSION__: JSON.stringify(require("./package.json").version)
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                parallel: true,
+                terserOptions: {
+                    format: {
+                        comments: false
+                    },
+                },
+                extractComments: false,
             }),
         ],
-        devtool: (argv.mode === "development") ? 'source-map' : false,
-    };
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            __VERSION__: JSON.stringify(require("./package.json").version)
+        }),
+    ]
 };
