a55c4b2059ce9ff49ea4b229838a681566356d51
Tim Dillon <tim@itsdits.com>
2023-11-18 09:27:37 +0900

    feat(project): improve generator
    * Add input validation
    * Adds new platform to Platform type
    * Adds entry in PlatformDomains

--- tools/generator.js
+++ tools/generator.js
@@ -1,81 +1,151 @@
 const fs = require('fs').promises;
 
+const platform = process.argv[2];
+
+// If any new modules are added, create a template for them and add them to this map.
+const sourcePath = 'tools/templates/';
 const filesMap = {
-    'achievements-template.txt': {
-        source: 'tools/templates/achievements-template.txt',
-        destination: 'src/achievements/impl/achievements-template.txt',
-    },
-    'ads-template.txt': {
-        source: 'tools/templates/ads-template.txt',
-        destination: 'src/ads/impl/ads-template.txt',
-    },
-    'context-template.txt': {
-        source: 'tools/templates/context-template.txt',
-        destination: 'src/context/impl/context-template.txt',
-    },
-    'core-template.txt': {
-        source: 'tools/templates/core-template.txt',
-        destination: 'src/core/impl/core-template.txt',
-    },
-    'iap-template.txt': {
-        source: 'tools/templates/iap-template.txt',
-        destination: 'src/iap/impl/iap-template.txt',
-    },
-    'leaderboard-template.txt': {
-        source: 'tools/templates/leaderboard-template.txt',
-        destination: 'src/leaderboard/impl/leaderboard-template.txt',
-    },
-    'notifications-template.txt': {
-        source: 'tools/templates/notifications-template.txt',
-        destination: 'src/notifications/impl/notifications-template.txt',
-    },
-    'player-template.txt': {
-        source: 'tools/templates/player-template.txt',
-        destination: 'src/player/impl/player-template.txt',
-    },
-    'session-template.txt': {
-        source: 'tools/templates/session-template.txt',
-        destination: 'src/session/impl/session-template.txt',
-    },
-    'stats-template.txt': {
-        source: 'tools/templates/stats-template.txt',
-        destination: 'src/stats/impl/stats-template.txt',
-    },
-    'tournament-template.txt': {
-        source: 'tools/templates/tournament-template.txt',
-        destination: 'src/tournament/impl/tournament-template.txt',
-    },
+    'achievements-template.txt': 'src/achievements/impl/achievements-template.txt',
+    'ads-template.txt': 'src/ads/impl/ads-template.txt',
+    'context-template.txt': 'src/context/impl/context-template.txt',
+    'core-template.txt': 'src/core/impl/core-template.txt',
+    'iap-template.txt': 'src/iap/impl/iap-template.txt',
+    'leaderboard-template.txt': 'src/leaderboard/impl/leaderboard-template.txt',
+    'notifications-template.txt': 'src/notifications/impl/notifications-template.txt',
+    'player-template.txt': 'src/player/impl/player-template.txt',
+    'session-template.txt': 'src/session/impl/session-template.txt',
+    'stats-template.txt': 'src/stats/impl/stats-template.txt',
+    'tournament-template.txt': 'src/tournament/impl/tournament-template.txt',
 };
 
-async function copyAndRenameFiles(platform) {
-    try {
-        for (const [fileName, { source, destination }] of Object.entries(filesMap)) {
-            await fs.copyFile(source, destination);
+const platformTypePath = 'src/session/types/session-types.ts';
+const platformTypeSearchString = 'export type Platform =';
 
-            const newFileName = destination
-                .replace('template', platform
-                .toLowerCase())
-                .replace('.txt', '.ts');
+const platformDomainsPath = 'src/data/core-data.ts';
+const platformDomainsSearchString = 'export const PLATFORM_DOMAINS: PlatformDomains = {';
 
-            await fs.rename(destination, newFileName);
+async function setupTemplates(platform) {
+    //TODO: find a more elegant way to do this, maybe a stream?
 
-            let fileContent = await fs.readFile(newFileName, 'utf-8');
-
-            fileContent = fileContent.replace(/TEMPLATE/g, platform);
-
-            await fs.writeFile(newFileName, fileContent, 'utf-8');
+    for (const [fileName, destination] of Object.entries(filesMap)) {
+        try {
+            await fs.copyFile(`${sourcePath}${fileName}`, destination);
+        } catch (error) {
+            console.error(`Failed to copy ${fileName} to ${destination}.`);
+            throw error;
         }
 
-        console.log('Files copied, renamed, and content modified successfully.');
-    } catch (error) {
-        console.error('Error:', error.message);
+        const newFileName = destination
+            .replace('template', platform.toLowerCase())
+            .replace('.txt', '.ts');
+
+        try {
+            await fs.rename(destination, newFileName);
+        } catch (error) {
+            console.error(`Failed to rename ${destination} to ${newFileName}.`);
+            throw error;
+        }
+
+        // This will update the class name and the header docs.
+        let fileContent;
+        try {
+            fileContent = await fs.readFile(newFileName, 'utf-8');
+        } catch (error) {
+            console.error(`Failed to read ${newFileName}.`);
+            throw error;
+        }
+
+        fileContent = fileContent.replace(/TEMPLATE/g, platform);
+
+        try {
+            await fs.writeFile(newFileName, fileContent, 'utf-8');
+        } catch (error) {
+            console.error(`Failed to write to ${newFileName}.`);
+            throw error;
+        }
     }
 }
 
-const platform = process.argv[2];
+async function addPlatformToPlatformType(filePath, searchString, newPlatform) {
+    let fileContent;
+    try {
+        fileContent = await fs.readFile(filePath, 'utf-8');
+    } catch (error) {
+        console.error(`Failed to read ${filePath}.`);
+        throw error;
+    }
+
+    // Locate the PlatformType definition in the file.
+    const searchRegex = new RegExp(searchString.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'), 'g');
+    const match = fileContent.match(searchRegex);
+    if (!match) {
+        console.error('Failed to find PlatformType definition.');
+        return;
+    }
+
+    // Would rather have this appended to the end of the type definition, but this was easier.
+    const modifiedContent = fileContent.replace(searchRegex, match[0] + ` "${newPlatform}" |`);
+
+    try {
+        await fs.writeFile(filePath, modifiedContent, 'utf-8');
+    } catch (error) {
+        console.error(`Failed to write to ${filePath}.`);
+        throw error;
+    }
+}
+
+async function addPlatformToPlatformDomains(filePath, searchString, newPlatform) {
+    let fileContent;
+    try {
+        fileContent = await fs.readFile(filePath, 'utf-8');
+    } catch (error) {
+        console.error(`Failed to read ${filePath}.`);
+        throw error;
+    }
+
+    // Locate the platform domains object in the file.
+    const searchRegex = new RegExp(searchString.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'), 's');
+    const match = fileContent.match(searchRegex);
+    if (!match) {
+        console.error('Failed to find PlatformDomains definition.');
+        return;
+    }
+
+    // Would rather have this appended to the end of the object, but this was easier.
+    const newProperty = `"${newPlatform}": [""],`;
+    const modifiedContent = fileContent.replace(searchRegex, match[0] + '\n' + '    ' + newProperty);
+
+    try {
+        await fs.writeFile(filePath, modifiedContent, 'utf-8');
+    } catch (error) {
+        console.error(`Failed to write to ${filePath}.`);
+        throw error;
+    }
+}
+
+function isValidPlatformName(platform) {
+    // Platform name should be PascalCase and contain only letters. This is because the platform name will be used
+    // to name the classes, and it's easier to force the string to lower for the file name than to force it to pascal
+    // case when we don't know what the platform name is or where word breaks will be.
+    const platformRegex = /^[A-Z][A-Za-z]*$/;
+
+    return platformRegex.test(platform);
+}
 
 if (!platform) {
-    console.error('Please provide the platform input.');
+    console.error('Platform name missing. Please provide a valid platform name as an argument.');
+} else if (!isValidPlatformName(platform)) {
+    console.error('Invalid platform name. Platform should contain only letters and be in PascalCase.');
 } else {
-    copyAndRenameFiles(platform);
+    setupTemplates(platform)
+        .then(() => console.log('Templates setup complete.'))
+        .catch(error => console.error('Error:', error.message));
+
+    addPlatformToPlatformType(platformTypePath, platformTypeSearchString, platform.toLowerCase())
+        .then(() => console.log('Platform appended to PlatformType definition.'))
+        .catch(error => console.error('Error:', error.message));
+
+    addPlatformToPlatformDomains(platformDomainsPath, platformDomainsSearchString, platform.toLowerCase())
+        .then(() => console.log('Platform appended to PlatformDomains definition.'))
+        .catch(error => console.error('Error:', error.message));
 }
