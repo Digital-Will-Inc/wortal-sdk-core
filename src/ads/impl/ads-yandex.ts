438e223067f01a8cfc70ab5c8045122d69d616aa
Tim Dillon <tim@itsdits.com>
2023-11-22 11:19:16 +0900

    fix(ads): analytics calls in yandex-ads impl

--- src/ads/impl/ads-yandex.ts
+++ src/ads/impl/ads-yandex.ts
@@ -34,12 +34,12 @@ export class AdsYandex extends AdsBase {
 
                 if (!status.stickyAdvIsShowing && shouldShow) {
                     Wortal._internalPlatformSDK.adv.showBannerAdv();
-                    this.logAdCall("banner", "pause", true);
+                    Wortal.analytics._logAdCall("banner", "pause", true);
                 }
             })
             .catch((error: any) => {
                 exception("Ad instance encountered an error or was not filled.", error);
-                this.logAdCall("banner", "pause", false);
+                Wortal.analytics._logAdCall("banner", "pause", false);
             });
     }
 
@@ -51,17 +51,17 @@ export class AdsYandex extends AdsBase {
                 },
                 onClose: (wasShown: boolean) => {
                     ad.callbacks.afterAd();
-                    this.logAdCall("interstitial", ad.placementType, wasShown);
+                    Wortal.analytics._logAdCall("interstitial", ad.placementType, wasShown);
                 },
                 onError: (error: unknown) => {
                     ad.callbacks.noFill();
                     exception("Ad instance encountered an error or was not filled.", error);
-                    this.logAdCall("interstitial", ad.placementType, false);
+                    Wortal.analytics._logAdCall("interstitial", ad.placementType, false);
                 },
                 onOffline: () => {
                     ad.callbacks.noFill();
                     warn("Ad instance not shown due to network error. Please check your internet connection.");
-                    this.logAdCall("interstitial", ad.placementType, false);
+                    Wortal.analytics._logAdCall("interstitial", ad.placementType, false);
                 }
             }
         });
@@ -75,13 +75,13 @@ export class AdsYandex extends AdsBase {
                 },
                 onClose: (wasShown: boolean) => {
                     ad.callbacks.afterAd();
-                    this.logAdCall("rewarded", ad.placementType, wasShown);
+                    Wortal.analytics._logAdCall("rewarded", ad.placementType, wasShown);
                 },
                 onError: (error: unknown) => {
                     ad.callbacks.noFill();
                     ad.callbacks.adDismissed?.();
                     exception("Ad instance encountered an error or was not filled.", error);
-                    this.logAdCall("rewarded", ad.placementType, false);
+                    Wortal.analytics._logAdCall("rewarded", ad.placementType, false);
                 },
                 onRewarded: () => {
                     ad.callbacks.adViewed?.();
