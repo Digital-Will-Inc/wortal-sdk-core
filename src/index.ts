2917ab8237023320070eaf284cc8b2694ffcd445
Tim Dillon <tim@itsdits.com>
2023-11-22 11:14:14 +0900

    refactor(core): handle platform detection for WARTs (#370)

--- src/index.ts
+++ src/index.ts
@@ -36,13 +36,7 @@ function determinePlatform(): Platform {
     for (const platform of Object.keys(PLATFORM_DOMAINS)) {
         if (PLATFORM_DOMAINS[platform].some(domain => host.includes(domain))) {
             if (platform === "wortal") {
-                // Some builds are hosted on Wortal domain but embedded elsewhere. These should include a query param
-                // with the platform name to determine the correct platform.
-                if (getParameterByName("telegram")) {
-                    return "telegram";
-                } else {
-                    return "wortal";
-                }
+                return parseURLParamsForPlatform();
             } else {
                 return platform as Platform;
             }
@@ -50,6 +44,24 @@ function determinePlatform(): Platform {
     }
 
     return "debug";
+}
+
+function parseURLParamsForPlatform(): Platform {
+    // Some builds are hosted on Wortal domain but embedded elsewhere, such as a WART bot.
+    // These should include a query param with the platform name to determine the correct platform.
+    if (getParameterByName("telegram")) {
+        return "telegram";
+    } else if (getParameterByName("telegram_bot")) {
+        // This is the WART bot on Telegram. It's a special case because the normal Telegram platform
+        // uses the Playdeck SDK, but WART does not. We'll initialize this as Wortal for now, but we
+        // need to sort out the details of this implementation.
+        //TODO: Figure out how to handle WART bot on Telegram
+        return "wortal";
+    } else if (getParameterByName("viber")) {
+        return "viber";
+    } else {
+        return "wortal";
+    }
 }
 
 export default Wortal;
